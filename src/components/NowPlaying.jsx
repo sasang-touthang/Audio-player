351389f25fc9546007250484ef19fe0725ac11ee
Lalngaisang Touthang <sasang@Sasangs-Machine.local>
2024-02-20 15:22:05 +0530

    second commit

--- src/components/NowPlaying.jsx
+++ src/components/NowPlaying.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useLayoutEffect, useRef } from "react";
+import React, { useEffect, useRef } from "react";
 
 function NowPlaying({ src, name, onEnded }) {
   // Reference the audio player
